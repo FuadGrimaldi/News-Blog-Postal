4382e1700fc96f1a183062aba91909c488a25461
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-06-21 16:17:45 +0700

    Add Connection

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const connectDb = require("./src/config/db");
 
+// Connection Database MongoDB
+connectDb();
 // Server
 const app = express();
 const port = 4000 || process.env.port;
