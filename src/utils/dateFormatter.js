e1171eb72be982d3ce29464fb182fdd1435b1f3c
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-06-24 21:11:17 +0700

    add helper formateDate

---
+++ src/utils/dateFormatter.js
@@ -0,0 +1,23 @@
+function formatDate(date) {
+  const months = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${date} ${month} ${year}`;
+}
+
+module.exports = formatDate;
